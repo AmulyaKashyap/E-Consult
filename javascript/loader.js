7fcb808b4d86fd4a29d32d641ab8ea5d558d849f
AmulyaKashyap <40742391+AmulyaKashyap@users.noreply.github.com>
2021-05-22 20:57:52 +0530

    Update loader.js

--- javascript/loader.js
+++ javascript/loader.js
@@ -7,33 +7,40 @@ $(document).ready(function(){
     $(".slider").slider();
     $('.scrollspy').scrollSpy();
     $('.sidenav').sidenav();
-    $('.datepicker').datepicker();
+    $('.datepicker').datepicker({
+        format:'yyyy-mm-dd'
+      });
+    
     $('.tabs').tabs();
+    $('.timepicker').timepicker({twelveHour:false});
     $('select').formSelect();
     $(".doctors").carousel({
         numVisible: 6,
         shift : 55,
         padding :20,
     });
+    $('.materialboxed').materialbox();
 });
-
-function toggleModal(){
-    var instance =M.Modal.getInstance($("#modal3"));
-    instance.open();
-}
 
 
 function toggleModal_signup(){
     var instance =M.Modal.getInstance($("#modalSign"));
     instance.open();
 }
-
 function toggleModal_doctor(){
     var instance =M.Modal.getInstance($("#asdoc"));
     instance.open();
 }
-function toggleModal_confirm(){
-    var instance =M.Modal.getInstance($("#confirm"));
+
+function toggleModal_login(){
+    var instance =M.Modal.getInstance($("#loginF"));
     instance.open();
 }
+function toggleModal_patient(){
+    var instance =M.Modal.getInstance($("#aspat"));
+    instance.open();
+}
+function goBack() {
+    window.history.back();}
 
+   
